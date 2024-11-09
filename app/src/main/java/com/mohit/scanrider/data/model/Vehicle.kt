e5fa9cc893e07bc3adfbbe79341e37a2acf4f72b
Mohit Sharma <118622195+mohitsharma16@users.noreply.github.com>
2024-11-09 11:01:47 +0530

    "vehicle data class"

---
+++ app/src/main/java/com/mohit/scanrider/data/model/Vehicle.kt
@@ -0,0 +1,9 @@
+package com.mohit.scanrider.data.model
+
+data class Vehicle(
+    val name: String,
+    val year: Int,
+    val model: String,
+    val auctionPrice: String,
+    val shippingCost: String
+)
